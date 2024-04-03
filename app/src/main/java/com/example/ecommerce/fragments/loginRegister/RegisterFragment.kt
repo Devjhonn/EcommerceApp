18190be8ae42ac1baf2d1d3e79187bf51dbd8c54
Devjhon <marcosjf20@gmail.com>
2024-04-03 15:05:14 -0300

    Confing auth firebase and implement navegation

--- app/src/main/java/com/example/ecommerce/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/ecommerce/fragments/loginRegister/RegisterFragment.kt
@@ -1,7 +1,67 @@
 package com.example.ecommerce.fragments.loginRegister
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.ecommerce.R
+import com.example.ecommerce.data.User
+import com.example.ecommerce.databinding.FragmentRegisterBinding
+import com.example.ecommerce.util.Resource
+import com.example.ecommerce.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class RegisterFragment:Fragment(R.layout.fragment_register) {
+private val TAG = "RegisterFragment"
+@Suppress("DEPRECATION")
+@AndroidEntryPoint
+class RegisterFragment:Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(inflater  )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonRegisterHaveRegister.setOnClickListener{
+                val user = User(
+                    edtNameRegister.text.toString().trim(),
+                    edtEmailRegister.text.toString().trim()
+                )
+                val password = edtPasswordRegister.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect{
+                when(it){
+                    is Resource.Loading -> {
+                        binding.buttonRegisterHaveRegister.startAnimation()
+                    }
+                    is Resource.Success ->{
+                        Log.d("test",it.data.toString())
+                        binding.buttonRegisterHaveRegister.revertAnimation()
+                    }
+                    is Resource.Error -> {
+                        Log.e(TAG,it.message.toString())
+                        binding.buttonRegisterHaveRegister.revertAnimation()
+                    }
+                }
+            }
+        }
+    }
 }
